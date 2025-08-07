c9bb9adb398ad6ea9778845b395b5fe07c59736b
linaigao <linaigao@berkeley.edu>
2025-08-07 16:36:12 -0700

    first commit

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export {default as NavItems} from './NavItems'
+export {default as MobileSidebar} from './MobileSideBar'
+export {default as Header} from './Header'
+export {default as StatsCard} from './StatsCard'
+export {default as TripCard} from './TripCard'
