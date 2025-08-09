f3c401ffbb3ef44307d7b967f6a9f8cc7def8d56
linaigao <linaigao@berkeley.edu>
2025-08-08 23:19:52 -0700

    Implement Trip Details Page

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
-export {default as NavItems} from './NavItems'
-export {default as MobileSidebar} from './MobileSideBar'
-export {default as Header} from './Header'
-export {default as StatsCard} from './StatsCard'
-export {default as TripCard} from './TripCard'
+export { default as NavItems } from "./NavItems";
+export { default as MobileSidebar } from "./MobileSideBar";
+export { default as Header } from "./Header";
+export { default as StatsCard } from "./StatsCard";
+export { default as TripCard } from "./TripCard";
+export { default as InfoPill } from "./InfoPill";
